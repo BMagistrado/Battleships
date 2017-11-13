56bed832405c0ed0528439399c0523c929f88385
brymag161 <brymag161@E421-022>
2017-11-13 11:06:33 +0000

    Objects
    1)Seperated class files anjd object files

---
+++ A2 Project/Objects/Player.vb
@@ -0,0 +1,52 @@
+﻿Public Class Player
+    Dim x As Integer
+    Dim y As Integer
+    Private playerName As String
+    Private playerShips(x, y) As Integer
+
+    Public Property Name() As String
+        Get
+            Return playerName
+        End Get
+        Set(value As String)
+            playerName = value
+        End Set
+    End Property
+
+    Public Property Ships(x, y) As Integer
+        Get
+            Return playerShips(x, y)
+        End Get
+        Set(value As Integer)
+            playerShips(x, y) = value
+
+        End Set
+    End Property
+
+    Public Property playerx As Integer
+        Get
+            Return x
+        End Get
+        Set(value As Integer)
+            If value > 10 Then
+                x = 10
+            Else
+                x = value
+            End If
+
+        End Set
+    End Property
+
+    Public Property playery As Integer
+        Get
+            Return y
+        End Get
+        Set(value As Integer)
+            If value > 10 Then
+                y = 10
+            Else
+                y = value
+            End If
+        End Set
+    End Property
+End Class
--- A2 Project/PvPMatch.vb
+++ A2 Project/PvPMatch.vb
@@ -45,59 +45,6 @@
         Loop While loopcounter <= 225
 
     End Sub
-    Public Class Player
-        Dim x As Integer
-        Dim y As Integer
-        Private playerName As String
-        Private playerShips(x, y) As Integer
-
-        Public Property Name() As String
-            Get
-                Return playerName
-            End Get
-            Set(value As String)
-                playerName = value
-            End Set
-        End Property
-
-        Public Property Ships(x, y) As Integer
-            Get
-                Return playerShips(x, y)
-            End Get
-            Set(value As Integer)
-                playerShips(x, y) = value
-
-            End Set
-        End Property
-
-        Public Property playerx As Integer
-            Get
-                Return x
-            End Get
-            Set(value As Integer)
-                If value > 10 Then
-                    x = 10
-                Else
-                    x = value
-                End If
-
-            End Set
-        End Property
-
-        Public Property playery As Integer
-            Get
-                Return y
-            End Get
-            Set(value As Integer)
-                If value > 10 Then
-                    y = 10
-                Else
-                    y = value
-                End If
-            End Set
-        End Property
-
-    End Class
 
     Private Sub btn_P1Enter_Click(sender As Object, e As EventArgs) Handles btn_P1Enter.Click
         Dim Player1 As New Player
