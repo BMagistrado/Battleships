8a479989b320fbbeff676f26b58ae1e966718e6c
brymag161 <brymag161@E421-022>
2017-11-13 11:36:24 +0000

    Ship Object File
    1) Added ship object
    2) Added attributes such as health, size and coordinates

---
+++ A2 Project/Objects/Ship.vb
@@ -0,0 +1,25 @@
+﻿Public Class Ship
+    Public Property shipX As Integer
+
+    Public Property shipY As Integer
+
+    Enum shipsizes As Byte
+        shipsmall = 2
+        shipmedium = 4
+        shiplarge = 6
+
+
+    End Enum
+
+
+    Public Property shipsize As shipsizes
+    Public Property health As Integer
+
+    Public Sub New(newshipsize As shipsizes)
+        shipsize = newshipsize
+
+        health = newshipsize / 2
+
+    End Sub
+
+End Class
